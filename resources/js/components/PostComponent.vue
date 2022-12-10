349ebecf9208dddf470d581fa70e62e171e2829c
Sultan <sultan.orunov@mail.ru>
2022-12-10 12:46:41 +0500

    Create first vue component

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="alert alert-info my-3">
+            Mi first component!
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
