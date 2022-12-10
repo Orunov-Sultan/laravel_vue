46b108bd17458b29655d0c8a94a24eeb5d24ae86
Sultan <sultan.orunov@mail.ru>
2022-12-10 13:44:29 +0500

    Property Data()

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -2,13 +2,33 @@
     <div class="container">
         <div class="alert alert-info my-3">
             Mi first component!
+            <SinglePostComponent></SinglePostComponent>
         </div>
     </div>
 </template>
 
 <script>
+import SinglePostComponent from "./SinglePostComponent";
+
 export default {
-    name: "PostComponent"
+    name: "PostComponent",
+
+    data() {
+        return {
+            name: 'Vasia',
+            age: 25,
+            skills: [
+                'html',
+                'css',
+                'js',
+                'php'
+            ]
+        }
+    },
+
+    components: {
+        SinglePostComponent
+    }
 }
 </script>
 
---
+++ resources/js/components/SinglePostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="alert alert-warning">
+    Single Post Component
+</div>
+</template>
+
+<script>
+export default {
+    name: "SinglePostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
