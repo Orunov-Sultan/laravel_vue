6cc074b8f806783f398de349bbe99eca74e96da3
Sultan <sultan.orunov@mail.ru>
2022-12-12 18:05:11 +0500

    Реализовал вывод списка персон

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -2,41 +2,21 @@
     <div class="container">
         <div class="alert alert-info my-3">
             <CreateComponent></CreateComponent>
-            <SinglePostComponent></SinglePostComponent>
-
-<!--            <table class="table">-->
-<!--                <thead class="table-dark">-->
-<!--                <tr>-->
-<!--                    <th scope="col">#</th>-->
-<!--                    <th scope="col">Name</th>-->
-<!--                    <th scope="col">Age</th>-->
-<!--                    <th scope="col">Job</th>-->
-<!--                </tr>-->
-<!--                </thead>-->
-<!--                <tbody>-->
-<!--                <tr v-for="person in persons">-->
-<!--                    <th scope="row">{{person.id}}</th>-->
-<!--                    <td>{{person.name}}</td>-->
-<!--                    <td>{{person.age}}</td>-->
-<!--                    <td>{{person.job}}</td>-->
-<!--                </tr>-->
-<!--                </tbody>-->
-<!--            </table>-->
-
+            <IndexComponent></IndexComponent>
         </div>
     </div>
 </template>
 
 <script>
-import SinglePostComponent from "./SinglePostComponent";
 import CreateComponent from "./CreateComponent";
+import IndexComponent from "./IndexComponent";
 
 export default {
     name: "PostComponent",
 
     data() {
         return {
-            persons: null
+
         }
     },
 
@@ -53,8 +33,8 @@ export default {
     },
 
     components: {
-        SinglePostComponent,
-        CreateComponent
+        CreateComponent,
+        IndexComponent
     }
 }
 </script>
