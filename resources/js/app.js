3b08a7aab6ebf15fd40f55ebb96c96e3ba63e178
Sultan <sultan.orunov@mail.ru>
2022-12-17 01:32:47 +0500

    Подключил Vue Router в Laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import router from "./router";
+import Index from "./components/Index"
 
 require('./bootstrap');
 
@@ -6,6 +8,7 @@ const app = new Vue({
     el: '#app',
 
     components: {
-
-    }
+        Index
+    },
+    router
 })
---
+++ resources/js/components/TagComponents.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    This is Tag component
+</div>
+</template>
+
+<script>
+export default {
+    name: "TagComponents"
+}
+</script>
+
+<style scoped>
+
+</style>
